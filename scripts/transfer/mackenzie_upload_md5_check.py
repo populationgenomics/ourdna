176d580d51db0d39097ca1c12d090ce32fb283d5
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-02-15 12:29:16 +1100

    Use summary

--- scripts/transfer/mackenzie_upload_md5_check.py
+++ scripts/transfer/mackenzie_upload_md5_check.py
@@ -6,6 +6,7 @@ from google.cloud import storage
 import sys
 
 BUCKET_NAME = 'cpg-mackenzie-main-upload'
+SUMMARY_FILE = 'garvan-mm-summary.txt'
 MANIFEST_SUFFIX = '/manifest.txt'
 FILENAME_COLUMN = 'filename'
 CHECKSUM_COLUMN = 'checksum'
@@ -17,20 +18,24 @@ def main():
 
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(BUCKET_NAME)
-    blobs = storage_client.list_blobs(BUCKET_NAME)
+
+    summary = bucket.get_blob(SUMMARY_FILE).download_as_string()
     any_mismatches = False
-    for blob in blobs:
+    for line in summary:
         # Skip anything that's not a manifest.
-        if not blob.name.endswith(MANIFEST_SUFFIX):
+        if not line.endswith(MANIFEST_SUFFIX):
             continue
 
+        # The summary lines contain the filename at the end, without any spaces.
+        manifest_filename = line.split(' ')[-1]
+
         # Read the manifest (TSV).
-        logging.info('reading manifest: {blob.name}')
-        manifest = blob.download_as_string()
+        logging.info('reading manifest: {manifest_filename}')
+        manifest = bucket.get_blob(manifest_filename).download_as_string()
         tsv_reader = csv.DictReader(manifest.splitlines(), delimiter='\t')
         for row in tsv_reader:
             # Filename is relative to the current directory.
-            full_filename = blob.name.split('/')[:-1] + '/' + row[FILENAME_COLUMN]
+            full_filename = manifest_filename.split('/')[:-1] + '/' + row[FILENAME_COLUMN]
             expected_md5 = row[CHECKSUM_COLUMN]
 
             # Read the checksum from the blob.

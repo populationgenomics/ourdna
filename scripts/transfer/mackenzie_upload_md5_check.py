20bcf29e79215fa6ab0e1268db266146c71ec669
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-02-15 13:26:25 +1100

    Handle non-existent blobs

--- scripts/transfer/mackenzie_upload_md5_check.py
+++ scripts/transfer/mackenzie_upload_md5_check.py
@@ -21,8 +21,13 @@ def main():
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(BUCKET_NAME)
 
-    summary = bucket.get_blob(SUMMARY_FILE).download_as_string()
-    any_mismatches = False
+    summary_blob = bucket.get_blob(SUMMARY_FILE)
+    if not summary_blob:
+        logging.error(f'blob does not exist: {SUMMARY_FILE}')
+        sys.exit(1)
+    summary = summary_blob.download_as_string()
+
+    any_errors = False
     for line in summary:
         # Skip anything that's not a manifest.
         if not line.endswith(MANIFEST_SUFFIX):
@@ -33,7 +38,14 @@ def main():
 
         # Read the manifest (TSV).
         logging.info('reading manifest: {manifest_filename}')
-        manifest = bucket.get_blob(manifest_filename).download_as_string()
+        manifest_blob = bucket.get_blob(manifest_filename)
+        if not manifest_blob:
+            logging.error(f'blob does not exist: {manifest_filename}')
+            any_errors = True
+            continue
+        manifest = manifest_blob.download_as_string()
+
+        # Check every file listed in the manifest.
         tsv_reader = csv.DictReader(manifest.splitlines(), delimiter='\t')
         for row in tsv_reader:
             # Filename is relative to the current directory.
@@ -42,15 +54,19 @@ def main():
 
             # Read the checksum from the blob.
             check_blob = bucket.get_blob(full_filename)
+            if not check_blob:
+                logging.error(f'blob does not exist: {full_filename}')
+                any_errors = True
+                continue
             actual_md5 = check_blob.md5_hash
 
             if expected_md5 == actual_md5:
                 logging.info(f'match: {full_filename}')
             else:
                 logging.error(f'mismatch: {full_filename}, {expected_md5=}, {actual_md5=}')
-                any_mismatches = True
+                any_errors = True
     
-    if any_mismatches:
+    if any_errors:
         sys.exit(1)
 
 

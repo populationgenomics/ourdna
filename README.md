825cc9f9a67332c53dda6cfe2c83f51036ca867f
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-02-15 18:11:50 +1100

    Add checksum script for Mackenzie's Mission upload (#1)
    * Add checksum script for Mackenzie's Mission upload
    
    * Use summary
    
    * Fix linter
    
    * More linter fixes
    
    * Depend on google-cloud-storage
    
    * Handle non-existent blobs
    
    * log format config
    
    * linter
    
    * chmod +x

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# CPG Python Template
+# OurDNA
 
-Template repository based on the [CPG team-docs](https://github.com/populationgenomics/team-docs/blob/main/new_repository.md) content.
-
-This contains python and markdown linting configuration, and a github linting action.
+This repository contains analysis code for the OurDNA cohort.
---
+++ scripts/transfer/mackenzie_upload_md5_check.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""Checks data transfer integrity by comparing MD5 checksums."""
+
+import csv
+import logging
+import sys
+from google.cloud import storage
+
+BUCKET_NAME = 'cpg-mackenzie-main-upload'
+SUMMARY_FILE = 'garvan-mm-summary.txt'
+MANIFEST_SUFFIX = '/manifest.txt'
+FILENAME_COLUMN = 'filename'
+CHECKSUM_COLUMN = 'checksum'
+
+
+def main():
+    """Main entrypoint."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(module)s:%(lineno)d - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        stream=sys.stderr,
+    )
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(BUCKET_NAME)
+
+    summary_blob = bucket.get_blob(SUMMARY_FILE)
+    if not summary_blob:
+        logging.error(f'blob does not exist: {SUMMARY_FILE}')
+        sys.exit(1)
+    summary = summary_blob.download_as_string()
+
+    any_errors = False
+    for line in summary:
+        # Skip anything that's not a manifest.
+        if not line.endswith(MANIFEST_SUFFIX):
+            continue
+
+        # The summary lines contain the filename at the end, without any spaces.
+        manifest_filename = line.split(' ')[-1]
+
+        # Read the manifest (TSV).
+        logging.info('reading manifest: {manifest_filename}')
+        manifest_blob = bucket.get_blob(manifest_filename)
+        if not manifest_blob:
+            logging.error(f'blob does not exist: {manifest_filename}')
+            any_errors = True
+            continue
+        manifest = manifest_blob.download_as_string()
+
+        # Check every file listed in the manifest.
+        tsv_reader = csv.DictReader(manifest.splitlines(), delimiter='\t')
+        for row in tsv_reader:
+            # Filename is relative to the current directory.
+            full_filename = (
+                manifest_filename.split('/')[:-1] + '/' + row[FILENAME_COLUMN]
+            )
+            expected_md5 = row[CHECKSUM_COLUMN]
+
+            # Read the checksum from the blob.
+            check_blob = bucket.get_blob(full_filename)
+            if not check_blob:
+                logging.error(f'blob does not exist: {full_filename}')
+                any_errors = True
+                continue
+            actual_md5 = check_blob.md5_hash
+
+            if expected_md5 == actual_md5:
+                logging.info(f'match: {full_filename}')
+            else:
+                logging.error(
+                    f'mismatch: {full_filename}, {expected_md5=}, {actual_md5=}'
+                )
+                any_errors = True
+
+    if any_errors:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
